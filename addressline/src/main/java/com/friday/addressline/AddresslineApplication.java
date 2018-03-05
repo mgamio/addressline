9e6cf85ddcc0d87e76fee18ab858e0528f40fa2c
Moises <mkgv89@hotmail.com>
2018-03-05 21:16:58 +0100

    initial commit

---
+++ addressline/src/main/java/com/friday/addressline/AddresslineApplication.java
@@ -0,0 +1,12 @@
+package com.friday.addressline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresslineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddresslineApplication.class, args);
+	}
+}
